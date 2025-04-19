d3fd44003a0eabd2b73181b6cacf49351ae8f343
Jon Ippolito <ron_elicit@yahoo.com>
2025-04-19 17:20:48 -0400

    Update app_picture_hanger.py
    Asked to omit LaTeX style codes.

--- app_picture_hanger.py
+++ app_picture_hanger.py
@@ -27,6 +27,8 @@ drop_to_hardware: This will be the distance between the top of the picture to th
 available_wall_width: This is the running length of wall space available to mount the picture; it is bounded by obstacles to the left and right such as wall corners, furniture, or other hangings.
 
 When asked to perform a calculation, please use Python to perform the calculation and follow the provided instructions EXACTLY.
+
+Do NOT type the LaTeX-style code for equations; either render formulas as mathematical symbols or omit santax like "\frac" and "\text" altogether.
 """
 
 PHASES = {
